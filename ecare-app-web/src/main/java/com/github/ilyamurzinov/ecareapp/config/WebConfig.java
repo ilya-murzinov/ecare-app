ecdccce4bcc4b3947ec337acaf9f878d6cc0463c
ilya-murzinov <murz42@gmail.com>
2014-12-02 14:45:56 +0300

    datasource: jta -> resource_local

--- ecare-app-web/src/main/java/com/github/ilyamurzinov/ecareapp/config/WebConfig.java
+++ ecare-app-web/src/main/java/com/github/ilyamurzinov/ecareapp/config/WebConfig.java
@@ -3,19 +3,14 @@ package com.github.ilyamurzinov.ecareapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.jta.JtaTransactionManager;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
 
 /**
  * @author ilya-murzinov
@@ -23,7 +18,7 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebMvc
 @EnableTransactionManagement
-@ComponentScan(basePackages = {"com.github.ilyamurzinov.ecareapp.web"})
+@ComponentScan(basePackages = {"com.github.ilyamurzinov.ecareapp"})
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -40,29 +35,18 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     }
 
     @Bean
-    public JtaTransactionManager getJtaTransactionManager() {
-        return new JtaTransactionManager();
+    public LocalEntityManagerFactoryBean getLocalEntityManagerFactoryBean() {
+        LocalEntityManagerFactoryBean localEntityManagerFactoryBean = new LocalEntityManagerFactoryBean();
+        localEntityManagerFactoryBean.setPersistenceUnitName("ecare");
+        return localEntityManagerFactoryBean;
     }
 
     @Bean
-    public DataSource getDataSource() throws NamingException {
-        InitialContext ctx = new InitialContext();
-        return (DataSource) ctx.lookup("ecare");
-    }
-
-    @Bean
-    public HibernateJpaVendorAdapter getJpaVendorAdapter() {
-        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
-        hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
-        hibernateJpaVendorAdapter.setShowSql(true);
-        return hibernateJpaVendorAdapter;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean getLocalContainerEntityManagerFactoryBean() throws NamingException {
-        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        localContainerEntityManagerFactoryBean.setJtaDataSource(getDataSource());
-        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(getJpaVendorAdapter());
-        return localContainerEntityManagerFactoryBean;
+    public JpaTransactionManager getJpaTransactionManager() {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setDataSource(getLocalEntityManagerFactoryBean().getDataSource());
+        jpaTransactionManager.setJpaDialect(new HibernateJpaDialect());
+        jpaTransactionManager.setEntityManagerFactory(getLocalEntityManagerFactoryBean().getNativeEntityManagerFactory());
+        return jpaTransactionManager;
     }
 }
